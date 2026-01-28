18bc1537db2634f17535a4c508d8b5dc2430fff7
Yoroi58 <melontart58.xafngo@gmail.com>
2026-01-28 09:12:23 +0000

    gone

--- index.js
+++ index.js
@@ -499,15 +499,34 @@ const repository = new Repository();
 
   let cityOfficeLocations = await repository.read("/cityOfficeLocations");
   let boundaries = await repository.read("/boundaries");
+  let areaList = {
+    "北海道地域" : ["北海道"],
+    "東北地域": ["青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",],
+    "関東地域": ["茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県"],
+    "北陸地域": ["新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県"],
+    "中部地域": ["岐阜県", "静岡県", "愛知県", "三重県"],
+    "近畿地域": ["滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県"],
+    "中国地域": ["鳥取県", "島根県", "岡山県", "広島県", "山口県"],
+    "四国地域": ["徳島県", "香川県", "愛媛県", "高知県"],
+    "九州地域": ["福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県"],
+    "沖縄地域": ["沖縄"]
+  }
   //repository.getCityOfficeLocations();
   selectedCities = cityOfficeLocations;
 
   controller = {
-    updateProblemMap: (cityName) =>{
+            updateProblemMap: (cityName) =>{
       if(!cityName) return;    
-        
+        const prefectureNames = areaList[cityName]; //
+        const areaPrefectures = prefectureNames?.group_by((prefName)=>prefName); //
+      
         const features = boundaries.features
-          .filter((feature) => feature.properties["N03_001"] == cityName)
+          .filter((feature) => { 
+            if(prefectureNames){ //
+              return areaPrefectures[feature.properties["N03_001"]] != null //
+            }else //
+              return feature.properties["N03_001"] == cityName
+          })
         
         const polylines = features.map((feature) => {
           if (feature.geometry.type == "Polygon") {
